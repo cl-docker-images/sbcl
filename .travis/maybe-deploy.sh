b1f28695e2a423056e0214a26bf3d01287ce0dee
Eric Timmons <etimmons@mit.edu>
2018-05-02 18:24:48 -0400

    Extract actual tags

--- .travis/maybe-deploy.sh
+++ .travis/maybe-deploy.sh
@@ -3,6 +3,9 @@
 if [ "x$TRAVIS_BRANCH" = "xrelease" ]; then
     docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 
-    docker push $(docker images -q daewok/sbcl)
+    TAGS=$(docker images daewok/sbcl | tail -n +2 | tr -s ' ' | cut -d ' ' -f 2)
+    for tag in ${TAGS}; do
+        docker push daewok/sbcl:${tag}
+    done
 
 fi
